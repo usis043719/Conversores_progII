9446c80c3ebc6d542a8190dae9b8068dab8e8330
usis043719 <gdadvid.dubon@gmail.com>
2020-02-18 16:38:47 -0600

    cominezo

---
+++ Conversores_progII/monedas.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Conversores_progII
+{
+    partial class monedas
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.lblRespuesta = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.cboDe = new System.Windows.Forms.ComboBox();
+            this.lblDe = new System.Windows.Forms.Label();
+            this.cboA = new System.Windows.Forms.ComboBox();
+            this.lblA = new System.Windows.Forms.Label();
+            this.cboTipo = new System.Windows.Forms.ComboBox();
+            this.lblcant = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.btnConvertir.Font = new System.Drawing.Font("Copperplate Gothic Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvertir.ForeColor = System.Drawing.Color.Black;
+            this.btnConvertir.Location = new System.Drawing.Point(183, 207);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(197, 38);
+            this.btnConvertir.TabIndex = 4;
+            this.btnConvertir.Text = "Convertir";
+            this.btnConvertir.UseVisualStyleBackColor = false;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // lblRespuesta
+            // 
+            this.lblRespuesta.AutoSize = true;
+            this.lblRespuesta.BackColor = System.Drawing.Color.Transparent;
+            this.lblRespuesta.Font = new System.Drawing.Font("Lato", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRespuesta.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.lblRespuesta.Location = new System.Drawing.Point(265, 178);
+            this.lblRespuesta.Name = "lblRespuesta";
+            this.lblRespuesta.Size = new System.Drawing.Size(20, 25);
+            this.lblRespuesta.TabIndex = 7;
+            this.lblRespuesta.Text = "_";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(219, 38);
+            this.txtCantidad.Multiline = true;
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(104, 20);
+            this.txtCantidad.TabIndex = 8;
+            this.txtCantidad.Text = "1";
+            this.txtCantidad.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // cboDe
+            // 
+            this.cboDe.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboDe.FormattingEnabled = true;
+            this.cboDe.Location = new System.Drawing.Point(124, 111);
+            this.cboDe.Name = "cboDe";
+            this.cboDe.Size = new System.Drawing.Size(108, 21);
+            this.cboDe.TabIndex = 10;
+            // 
+            // lblDe
+            // 
+            this.lblDe.AutoSize = true;
+            this.lblDe.Font = new System.Drawing.Font("Copperplate Gothic Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDe.ForeColor = System.Drawing.Color.Black;
+            this.lblDe.Location = new System.Drawing.Point(73, 104);
+            this.lblDe.Name = "lblDe";
+            this.lblDe.Size = new System.Drawing.Size(44, 21);
+            this.lblDe.TabIndex = 9;
+            this.lblDe.Text = "De:";
+            // 
+            // cboA
+            // 
+            this.cboA.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboA.FormattingEnabled = true;
+            this.cboA.Location = new System.Drawing.Point(357, 110);
+            this.cboA.Name = "cboA";
+            this.cboA.Size = new System.Drawing.Size(110, 21);
+            this.cboA.TabIndex = 12;
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Font = new System.Drawing.Font("Copperplate Gothic Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblA.ForeColor = System.Drawing.Color.Black;
+            this.lblA.Location = new System.Drawing.Point(319, 104);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(31, 21);
+            this.lblA.TabIndex = 11;
+            this.lblA.Text = "A:";
+            // 
+            // cboTipo
+            // 
+            this.cboTipo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboTipo.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboTipo.FormattingEnabled = true;
+            this.cboTipo.Location = new System.Drawing.Point(442, 240);
+            this.cboTipo.Name = "cboTipo";
+            this.cboTipo.Size = new System.Drawing.Size(105, 26);
+            this.cboTipo.TabIndex = 13;
+            this.cboTipo.Visible = false;
+            this.cboTipo.SelectedIndexChanged += new System.EventHandler(this.cboTipo_SelectedIndexChanged);
+            // 
+            // lblcant
+            // 
+            this.lblcant.AutoSize = true;
+            this.lblcant.Font = new System.Drawing.Font("Maiandra GD", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcant.Location = new System.Drawing.Point(75, 38);
+            this.lblcant.Name = "lblcant";
+            this.lblcant.Size = new System.Drawing.Size(138, 18);
+            this.lblcant.TabIndex = 14;
+            this.lblcant.Text = "Ingrese cantidad :\r\n";
+            // 
+            // monedas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 278);
+            this.Controls.Add(this.lblcant);
+            this.Controls.Add(this.cboTipo);
+            this.Controls.Add(this.cboA);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.cboDe);
+            this.Controls.Add(this.lblDe);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.lblRespuesta);
+            this.Controls.Add(this.btnConvertir);
+            this.Name = "monedas";
+            this.Text = "Conversor de Monedas";
+            this.Load += new System.EventHandler(this.monedas_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConvertir;
+        private System.Windows.Forms.Label lblRespuesta;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.ComboBox cboDe;
+        private System.Windows.Forms.Label lblDe;
+        private System.Windows.Forms.ComboBox cboA;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.ComboBox cboTipo;
+        private System.Windows.Forms.Label lblcant;
+    }
+}
+
